e8b9d09e85cd5b38396118e27ea29469aa9acf09
bailiebutcher <bailiebutcher@gmail.com>
2017-10-24 15:07:03 -0600

    Added some Javascript

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 "use strict";
 
-function todoList(){
-    var item=document.getElementById("todoInput").value;
-    var text=document.createTextNode(item);
-    var newItem=document.createElement("li");
-    newItem.appendChild(text);
-    document.getElementById("todoList").appendChild(newItem)
+function listName(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("listNameInput").innerHTML.value;
+    var node = document.createTextNode(inputValue);
+    li.appendChild(node);
+
+    if(inputValue === " "){
+        alert("Please Enter Task");
+    } else{
+        document.getElementById("list").appendChild(li);
+    }
+    document.getElementById("listNameInput").value = "";
+}
+
+
+
+var list = document.querySelector('ul');
+document.getElementById("submit-button").addEventListener('click', function(ev){
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+    var nodeList = document.getElementsByTagName("LI");
+    var i;
+    for (i = 0; i < nodeList.length; i++){
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        nodeList[i].appendChild(span);
+
 }
